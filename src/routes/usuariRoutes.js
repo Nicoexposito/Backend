fd60d35b1b8566a4d0387840c853756f95ef3d3f
Nicoexposito <niexal@jviladoms.cat>
2025-11-18 10:39:55 +0100

    index.js

--- src/routes/usuariRoutes.js
+++ src/routes/usuariRoutes.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const usuariController = require('../controllers/usuariController');
 
-// Registre i Login
+// RUTAS FIJAS (Registro y Login) – deben ir primero
 router.post('/registre', usuariController.registre);
 router.post('/login', usuariController.login);
 
-// CRUD
+// RUTAS CRUD
 router.post('/', usuariController.createUsuari);
 router.get('/', usuariController.getUsuaris);
+
+// RUTAS DINÁMICAS CON PARAMETRO ID
 router.get('/:id', usuariController.getUsuariById);
 router.put('/:id', usuariController.updateUsuari);
 router.delete('/:id', usuariController.deleteUsuari);
